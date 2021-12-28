b980d6bab9ad0214c7aea65494aebee431703294
Chenyh64 <chenyh64@gmail.com>
2021-12-28 17:08:03 -0500

    add pattern graph generation in main

--- main.cpp
+++ main.cpp
@@ -9,29 +9,62 @@
 
 using namespace std;
 
+// XXX: Move the following pattern graph generation func to a lib
+// This cycle here is the "cylce" that excludes swap cycle.
+Graph QAOALinearPattern(int n, int cycle=INT_MAX) {
+    Graph g(n, false);
+    vector<int> pi;
+    for (int i = 0; i < n; i++) pi.emplace_back(i);
+
+    int maxCycle = n % 2 == 0 ? 2 * n - 2 : 2 * n - 4;
+    int curCycle = 0;
+    for (int t = 0; t < maxCycle; t++) {
+        if (t % 4 == 0) {
+            for (int p = 0; p < n - 1; p += 2) {
+                g.add_edge(pi[p], pi[p + 1]);
+            }
+        } else if (t % 4 == 1) {
+            for (int p = 1; p < n - 1; p += 2) {
+                g.add_edge(pi[p], pi[p + 1]);
+            }
+        } else if (t % 4 == 2) {
+            for (int p = 1; p < n - 1; p += 2) {
+                swap(pi[p], pi[p + 1]);
+            }
+        } else if (t % 4 == 3) {
+            for (int p = 0; p < n - 1; p += 2) {
+                swap(pi[p], pi[p + 1]);
+            }
+        }
+        curCycle++;
+        if (curCycle == cycle) return g;
+    }
+    if (n % 2 == 1) {
+        for (int p = 1; p < n - 1; p += 2) {
+            swap(pi[p], pi[p + 1]);
+        }
+        for (int p = 0; p < n - 1; p += 2) {
+            g.add_edge(pi[p], pi[p + 1]);
+        }
+    }
+    return g;
+}
+
 int main(int, char**) {
-    ifstream qgraph_file("../test/graph/triangle.txt");
-    ifstream dgraph_file("../test/graph/10_1_0.txt");
-    if (!qgraph_file.is_open()) {
-        cerr << "query graph file triangle.txt does not exist.\n";
-        return(EXIT_FAILURE);
+    Graph queryGraph;
+    string queryGraphPath = "../../test/graph/10_1_0.txt";
+    ifstream queryGraphFile(queryGraphPath);
+    if (!queryGraphFile.is_open() || !queryGraph.load_from_file(queryGraphFile)) {
+        cerr << "Cannot load query graph file ../../test/graph/10_1_0.txt.\n";
+        return -1;
     }
-    if (!dgraph_file.is_open()) {
-        cerr << "data graph file 10_1_0.txt does not exist.\n";
-        return(EXIT_FAILURE);        
-    }
+    queryGraphFile.close();
 
-    Graph qgraph, dgraph;
-    if (!qgraph.load_from_file(qgraph_file)) {
-        cerr << "Cannot load graph file triangle.txt.\n";
-    }
-    if (!dgraph.load_from_file(dgraph_file)) {
-        cerr << "Cannot load graph file 10_1_0.txt.\n";
-    }
+    Graph dataGraph = QAOALinearPattern(10); // Pattern graph
 
-    GraphMatch gm(qgraph, dgraph);
+    GraphMatch gm(queryGraph, dataGraph);
     vector<unordered_map<int, int>> result;
-    result = gm.subgraph_isomorphsim();
+    result = gm.subgraph_isomorphsim(1);
     cout << "subgraph isomorphsim finds " << result.size() << " results" << endl;
     for (int i = 0; i < result.size(); i++) {
         cout << "subgraph " << i << "(d - q):" << endl;
@@ -41,8 +74,6 @@ int main(int, char**) {
         cout << endl;
     }
 
-    qgraph_file.close();
-    dgraph_file.close();
 
     return 0;
 }
--- test/subiso_test.cpp
+++ test/subiso_test.cpp
@@ -64,6 +64,38 @@ TEST_F(SubIsoTest, Figure1Test) {
 }
 
 
+TEST_F(SubIsoTest, TriangleTest) {
+    Graph queryG;
+    string queryGraphPath = "../../test/graph/triangle.txt";
+    ifstream queryGraphFile(queryGraphPath);
+    if (!queryGraphFile.is_open() || !queryG.load_from_file(queryGraphFile)) {
+        cerr << "Cannot load query graph file ../../test/graph/triangle.txt.\n";
+        FAIL();
+    }
+    Graph dataG;
+    string dataGraphPath = "../../test/graph/fig1d.txt";
+    ifstream dataGraphFile(dataGraphPath);
+    if (!dataGraphFile.is_open() || !dataG.load_from_file(dataGraphFile)) {
+        cerr << "Cannot load query graph file ../../test/graph/fig1d.txt.\n";
+        FAIL();
+    }
+
+    GraphMatch gm(queryG, dataG);
+    vector<unordered_map<int, int>> result;
+    result = gm.subgraph_isomorphsim();
+    cout << "subgraph isomorphsim finds " << result.size() << " results" << endl;
+    for (int i = 0; i < result.size(); i++) {
+        auto M_prime = result[i];
+        // For each edge u u' in the queryG, we can find edge M[u], M[u'] in dataG
+        for (auto edge : queryG.get_edges()) {
+            int u = edge[0], u_prime = edge[1];
+            EXPECT_EQ(dataG.has_edge(M_prime[u], M_prime[u_prime]), true);
+        }
+    }
+}
+
+
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
